70b793289bb8cbaf9f63cd968d9903d72f35d26c
JoaoMatheusdeJesusMendes <jm2matheus@gmail.com>
2026-04-29 13:14:16 -0300

    Atualizações: Dropdown para versão mobile, ajustes de bordas de feedback para versões mobile

--- site_mestre/src/components/Navbar.vue
+++ site_mestre/src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { ref, onMounted, onBeforeUnmount } from "vue"
 import { getBrandInfo } from '../services/brandService'
 
+const isOpen = ref(true)
+const root = ref<HTMLElement | null>(null)
 const { name, logo } = getBrandInfo()
 
 const scrollToSection = (SectionId: string) => {
@@ -9,16 +12,37 @@ const scrollToSection = (SectionId: string) => {
     section.scrollIntoView({ behavior: 'smooth'})
   }
 }
+
+const handleOutsideClick = (e: MouseEvent) => {
+  const target = e.target as Node
+
+  if (root.value && !root.value.contains(target)) {
+    isOpen.value = false
+  }
+}
+
+onMounted(() => {
+  document.addEventListener("click", handleOutsideClick)
+})
+
+onBeforeUnmount(() => {
+  document.removeEventListener("click", handleOutsideClick)
+})
+
 </script>
 
 <template>
-  <nav class="navbar">
+  <nav class="navbar" ref="root">
     <div class="logo-area">
       <img :src="logo" alt="Logo Mestre Biel" class="logo" />
       <span class="brand-name">{{ name }}</span>
     </div>
 
-    <div class="actions">
+    <button class="menu-toggle" @click="isOpen = !isOpen">
+        ☰
+    </button>
+
+    <div class="actions" v-show="isOpen">
       <button class="btn-nav" @click="scrollToSection('about')">Quem eu sou?</button>
       <button class="btn-nav" @click="scrollToSection('feedback')">Sussuros da taverna</button>
       <button class="btn-nav" @click="scrollToSection('services')">Onde posso leva-lo?</button>
